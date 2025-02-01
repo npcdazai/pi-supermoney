9dee0141a6cdaa22751e04b93270e9d2e363c9be
npcdazai <prathammandavkar932@gmail.com>
2025-02-01 16:29:37 +0530

    CheckBox

--- src/components/ui/checkbox.jsx
+++ src/components/ui/checkbox.jsx
@@ -6,8 +6,8 @@ export const Checkbox = React.forwardRef(function Checkbox(props, ref) {
   return (
     <ChakraCheckbox.Root ref={rootRef} {...rest}>
       <ChakraCheckbox.HiddenInput ref={ref} {...inputProps} />
-      <ChakraCheckbox.Control>
-        {icon || <ChakraCheckbox.Indicator />}
+      <ChakraCheckbox.Control border="1px solid #D0D5DD" borderRadius="md" >
+        {icon || <ChakraCheckbox.Indicator color="#101828" />}
       </ChakraCheckbox.Control>
       {children != null && (
         <ChakraCheckbox.Label>{children}</ChakraCheckbox.Label>
